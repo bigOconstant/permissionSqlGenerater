33aa570f54399e541f4bed574831410613aaad5d
Caleb McCarthy <caleb.a.mccarthy@gmail.com>
2021-04-08 21:03:49 -0500

    switching pcs

--- main.cpp
+++ main.cpp
@@ -2,7 +2,9 @@
 #include <sstream>
 #include <fstream>
 #include <string>
+#include <utility>
 #include <map>
+#include <vector>
 
 using namespace std;
 
@@ -14,7 +16,7 @@ private:
   int idIterator;
 
 public:
-  RoleList(){
+  BaseList(){
     idIterator = 1;
   }
   int InsertOrGet(string name) {
@@ -25,21 +27,76 @@ public:
     
     this->idMap[idIterator] = name;
     this->nameMap[name] = idIterator;
+    int retVal = idIterator;
     ++idIterator;
+    return retVal;
   };
   
   
 };
 
+struct RoleItem{
+    string name;
+    int id;
+    std::vector<std::pair<string,string>> Permissions;
+};
+
+BaseList CreateRoleList(){
+  string line;
+  ifstream infile("data.txt");
+  BaseList retVal;
+  int RoleItr = 0;
+  while (std::getline(infile,line))
+    {
+      if (line.find("[role]") != std::string::npos) {
+          RoleItr = 0;
+      }else {
+        if (RoleItr == 1) {
+          retVal.InsertOrGet(line);
+        }
+      }
+      ++RoleItr;
+    }
+  return retVal;
+};
+
+BaseList CreateResourceList() {
+  string line;
+  ifstream infile("data.txt");
+  BaseList retVal;
+  bool startRead = false;
+  while (std::getline(infile,line))
+    {
+      if (line.find("[permissions]") != std::string::npos) {
+           cout<<"Setting to true"<<endl;
+          startRead = true;
+      }else if (line == "") {
+        cout<<"setting to false"<<endl;
+        startRead = false;
+      } else {
+        if (startRead == true) {
+          cout<<"inserting:"<<line<<endl;
+          cout<<line.substr(0,line.find(":"))<<endl;
+          retVal.InsertOrGet(line.substr(0,line.find(":")));
+        }
+      }
+      
+    }
+    return retVal;
+}
 
 int main() {
+  std::vector<RoleItem> RoleList;
+  BaseList Roles = CreateRoleList();
+  BaseList Resources = CreateResourceList();
+
   cout<<"hello world"<<endl;
   string line;
   ifstream infile("data.txt");
-  while (std::getline(infile,line))
-    {
-      std::istringstream iss(line);
-      cout<<line<<endl;
-    }
+  // while (std::getline(infile,line))
+  //   {
+  //     std::istringstream iss(line);
+  //     cout<<line<<endl;
+  //   }
   return 0;
 }
